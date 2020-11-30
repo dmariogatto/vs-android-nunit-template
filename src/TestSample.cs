aea8fd8b94ee66f9ae4054b09bd6ba04050a3b5d
Daniel Gatto <d.mario.gatto@gmail.com>
2020-11-30 18:32:26 +1030

    added template

---
+++ src/TestSample.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace $safeprojectname$
+{
+    [TestFixture]
+    public class TestSample
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // write your test fixture setup
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // write your test fixture teardown 
+        }
+
+        [Test]
+        public void Pass()
+        {
+            Console.WriteLine("test1");
+            Assert.True(true);
+        }
+
+        [Test]
+        public void Fail()
+        {
+            Assert.False(true);
+        }
+
+        [Test]
+        [Ignore("another time")]
+        public void Ignore()
+        {
+            Assert.False(true);
+        }
+
+        [Test]        
+        public void Inconclusive()
+        {
+            Assert.Inconclusive(nameof(Inconclusive));
+        }
+    }
+}
